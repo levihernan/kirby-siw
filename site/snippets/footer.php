d99015ebd1846af771b619295d3b28216b01a181
levihernan <levi.hernan@gmail.com>
2019-05-13 22:32:29 -0300

    footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -4,22 +4,22 @@
     <div class="row mb-5">
       <div class="col-lg-3 col-md-6 mb-5 mb-md-5 ftco-animate">
         <div class="ftco-footer-widget mb-4">
-          <img class="footer-logo" src="<? $kirby->url('assets') ?>/assets/images/logos/argentinalogo.jpg" alt="">
+          <img class="footer-logo" src="<?= $site->url() ?>/assets/images/logos/argentinalogo.jpg" alt="">
         </div>
       </div>
       <div class="col-lg-3 col-md-6 mb-5 mb-md-5 ftco-animate">
         <div class="ftco-footer-widget mb-4">
-          <img class="footer-logo" src="<? $kirby->url('assets') ?>/assets/images/logos/g20logo.png" alt="">
+          <img class="footer-logo" src="<?= $site->url() ?>/assets/images/logos/g20logo.png" alt="">
         </div>
       </div>
       <div class="col-lg-3 col-md-6 mb-5 mb-md-5 ftco-animate">
         <div class="ftco-footer-widget mb-4">
-          <img class="footer-logo" src="<? $kirby->url('assets') ?>/assets/images/logos/oijlogo.png" alt="">
+          <img class="footer-logo" src="<?= $site->url() ?>/assets/images/logos/oijlogo.png" alt="">
         </div>
       </div>
       <div class="col-lg-3 col-md-6 mb-5 mb-md-5 ftco-animate">
         <div class="ftco-footer-widget mb-4">
-          <img class="footer-logo" src="<? $kirby->url('assets') ?>/assets/images/logos/undp.png" alt="">
+          <img class="footer-logo" src="<?= $site->url() ?>/assets/images/logos/undp.png" alt="">
         </div>
       </div>
 
@@ -41,23 +41,23 @@
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
-  <script src="<? $kirby->url('assets') ?>/assets/js/jquery.min.js"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/popper.min.js"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/bootstrap.min.js"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/jquery.easing.1.3.js"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/jquery.waypoints.min.js"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/jquery.stellar.min.js"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/owl.carousel.min.js"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/jquery.magnific-popup.min.js"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/aos.js"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/jquery.animateNumber.min.js"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/bootstrap-datepicker.js"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/jquery.timepicker.min.js"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/scrollax.min.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/jquery.min.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/popper.min.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/bootstrap.min.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/jquery.easing.1.3.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/jquery.waypoints.min.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/jquery.stellar.min.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/owl.carousel.min.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/jquery.magnific-popup.min.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/aos.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/jquery.animateNumber.min.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/bootstrap-datepicker.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/jquery.timepicker.min.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/scrollax.min.js"></script>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/google-map.js"></script>
-  <script src="<? $kirby->url('assets') ?>/assets/js/main.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/google-map.js"></script>
+  <script src="<?= $site->url() ?>/assets/js/main.js"></script>
 
 </body>
 </html>
