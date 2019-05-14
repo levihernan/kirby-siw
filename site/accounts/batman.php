9753f6337f20f2ce330447016aeac9366797a3ee
levihernan <levi.hernan@gmail.com>
2019-05-13 22:40:10 -0300

    users

---
+++ site/accounts/batman.php
@@ -0,0 +1,13 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: batman
+password: >
+  $2y$10$5IEjE2txOo3d6aXm5GeJV.x4S.kdhH96AI3AtLKCgs7Ck5PtmjPMm
+email: nan@maran.co
+language: en
+role: admin
+history:
+  - capacity-building-programme
+  - home
+  - proyectos/proyecto-1
+  - proyectos/proyecto-2
